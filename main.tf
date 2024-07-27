8298f8429ef998af171f1210f3b9063b937af7e2
pcguywilson <pcguywilson@gmail.com>
2024-07-27 17:03:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# Define a tag map
 locals {
   common_tags = {
     Owner = "Swilson"
@@ -27,19 +26,119 @@ resource "aws_iam_role" "lambda_role" {
   tags = local.common_tags
 }
 
-# Attach policy to Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Package and deploy Lambda function
+# Package Lambda function code
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda"
   output_path = "${path.module}/lambda_function.zip"
 }
 
+# Create an S3 bucket for CodeBuild
+resource "aws_s3_bucket" "codebuild_bucket" {
+  bucket = "codebuild-bucket-${random_id.bucket_id.hex}"
+  tags   = local.common_tags
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+# Upload buildspec and tests to S3
+resource "aws_s3_bucket_object" "buildspec" {
+  bucket = aws_s3_bucket.codebuild_bucket.bucket
+  key    = "buildspec.yml"
+  source = "${path.module}/buildspec.yml"
+}
+
+resource "aws_s3_bucket_object" "lambda_code" {
+  bucket = aws_s3_bucket.codebuild_bucket.bucket
+  key    = "lambda/lambda_function.py"
+  source = "${path.module}/lambda/lambda_function.py"
+}
+
+resource "aws_s3_bucket_object" "tests" {
+  bucket = aws_s3_bucket.codebuild_bucket.bucket
+  key    = "tests/test_lambda_function.py"
+  source = "${path.module}/tests/test_lambda_function.py"
+}
+
+# Create IAM role for CodeBuild
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+      }
+    ]
+  })
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+# Create CodeBuild project
+resource "aws_codebuild_project" "lambda_tests" {
+  name           = "lambda-tests"
+  service_role   = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type            = "S3"
+    location        = "${aws_s3_bucket.codebuild_bucket.bucket}/buildspec.yml"
+    buildspec       = "buildspec.yml"
+  }
+  tags = local.common_tags
+}
+
+# Trigger CodeBuild to run the tests
+resource "null_resource" "trigger_codebuild" {
+  provisioner "local-exec" {
+    command = "aws codebuild start-build --project-name ${aws_codebuild_project.lambda_tests.name}"
+  }
+  depends_on = [aws_codebuild_project.lambda_tests]
+}
+
+# Wait for CodeBuild to complete and check the build status
+data "aws_codebuild_build" "latest_build" {
+  project_name = aws_codebuild_project.lambda_tests.name
+  depends_on   = [null_resource.trigger_codebuild]
+}
+
+resource "null_resource" "wait_for_tests" {
+  provisioner "local-exec" {
+    command = <<EOT
+      STATUS=$(aws codebuild batch-get-builds --ids ${data.aws_codebuild_build.latest_build.id} --query 'builds[0].buildStatus' --output text)
+      if [ "$STATUS" != "SUCCEEDED" ]; then
+        echo "Tests failed, aborting deployment."
+        exit 1
+      fi
+    EOT
+  }
+  depends_on = [data.aws_codebuild_build.latest_build]
+}
+
+# Deploy Lambda function only if tests pass
 resource "aws_lambda_function" "brewery_lambda" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "brewery-lambda"
@@ -56,22 +155,16 @@ resource "aws_lambda_function" "brewery_lambda" {
       STATE = "Ohio"
     }
   }
+  depends_on = [null_resource.wait_for_tests]
 }
 
-# Create CloudWatch Log Group for Lambda
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
   name              = "/aws/lambda/${aws_lambda_function.brewery_lambda.function_name}"
   retention_in_days = 14
   tags              = local.common_tags
+  depends_on        = [aws_lambda_function.brewery_lambda]
 }
 
-# Create CloudWatch Log Stream for Lambda
-resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
-  name           = "log-stream"
-  log_group_name = aws_cloudwatch_log_group.lambda_log_group.name
-}
-
-# Allow Lambda to create log groups and streams
 resource "aws_iam_policy" "lambda_logging_policy" {
   name   = "lambda-logging-policy"
   policy = jsonencode({
