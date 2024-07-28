b0c35f7bc44a3f4e33c8f6695c10d2a5baf44145
pcguywilson <pcguywilson@gmail.com>
2024-07-28 10:57:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,21 +55,14 @@ resource "aws_iam_role_policy_attachment" "lambda_logging_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_logging_policy.arn
 }
 
-resource "null_resource" "pip_install" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      mkdir build &&
-      copy lambda_function.py build\ &&
-      cd build &&
-      powershell Compress-Archive -Path * -DestinationPath ..\lambda_function.zip &&
-      cd .. &&
-      rmdir /s /q build
-    EOT
-  }
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "brewery_lambda" {
-  filename         = "${path.module}/lambda_function.zip"
+  filename         = data.archive_file.lambda_zip.output_path
   function_name    = "brewery-lambda"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
