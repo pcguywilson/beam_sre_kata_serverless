df4a4f65ee14f2c7632a20e42d54a82f17ad2d68
pcguywilson <pcguywilson@gmail.com>
2024-07-28 10:26:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ locals {
   }
 }
 
+# IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
   assume_role_policy = jsonencode({
@@ -25,40 +26,19 @@ resource "aws_iam_role" "lambda_role" {
   tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
+# Attach policies to the role
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "brewery_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "brewery-lambda"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.8"
-  timeout          = 60
-  tags             = local.common_tags
-
-  environment {
-    variables = {
-      CITY  = "Columbus"
-      STATE = "Ohio"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.brewery_lambda.function_name}"
-  retention_in_days = 14
-  tags              = local.common_tags
-}
-
-locals {
-  lambda_logging_policy_name = "lambda-logging-policy-${replace(formatdate("YYYYMMDDhhmmss", timestamp()), ":", "-")}"
+resource "aws_iam_role_policy_attachment" "lambda_logging_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging_policy.arn
 }
 
 resource "aws_iam_policy" "lambda_logging_policy" {
-  name   = local.lambda_logging_policy_name
+  name   = "lambda-logging-policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -73,19 +53,43 @@ resource "aws_iam_policy" "lambda_logging_policy" {
       }
     ]
   })
+  tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_logging_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+# Create Lambda function
+resource "aws_lambda_function" "brewery_lambda" {
+  filename         = "${path.module}/lambda_function.zip"
+  function_name    = "brewery-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  timeout          = 60
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
+  tags             = local.common_tags
+
+  environment {
+    variables = {
+      CITY  = "Columbus"
+      STATE = "Ohio"
+    }
+  }
 }
 
+# Create CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.brewery_lambda.function_name}"
+  retention_in_days = 14
+  tags              = local.common_tags
+}
+
+# Create Lambda deployment package
 resource "null_resource" "pip_install" {
   provisioner "local-exec" {
-    command = <<-EOT
+    command = <<EOT
       mkdir -p ./python &&
-      cp ./lambda_function.py ./python/ &&
-      cd ./python && zip -r ../lambda_function.zip .
+      pip install -r requirements.txt -t ./python &&
+      cp lambda_function.py ./python/ &&
+      cd python && zip -r ../lambda_function.zip .
     EOT
   }
 }
